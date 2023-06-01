907a31a0bc60009bc73be554b5ac834a3e5d6a12
RulaHisham <duraidirula@gmail.com>
2023-06-01 23:42:41 +0300

    changs

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class About {
+
+ render(){
+    return (
+        <div>About</div>
+      )
+ }
+ 
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Home {
+
+    
+
+    render(){
+        return (
+            <div>Home</div>
+          )
+     }
+}
